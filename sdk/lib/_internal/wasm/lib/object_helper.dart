c6e29a2eab0dd1d0e1ea5a4e1e688d8320ea4f2f
Ömer Sinan Ağacan <omersa@google.com>
2023-07-24 14:38:44 +0200

    Fixups

--- sdk/lib/_internal/wasm/lib/object_helper.dart
+++ sdk/lib/_internal/wasm/lib/object_helper.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+library dart._object_helper;
+
 // Access hidden identity hash code field.
 external int getHash(Object obj);
 external void setHash(Object obj, int hash);
