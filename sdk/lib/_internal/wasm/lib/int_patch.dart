c3ccbdfcbfd117548903081efd56aeacf721c57f
Ömer Sinan Ağacan <omersa@google.com>
2023-08-10 09:46:42 +0200

    Formatting

--- sdk/lib/_internal/wasm/lib/int_patch.dart
+++ sdk/lib/_internal/wasm/lib/int_patch.dart
@@ -299,21 +299,21 @@ class int {
     return _int64OverflowLimits[tableIndex];
   }
 
-/// Wasm i64.div_s instruction
-external int _div_s(int divisor);
+  /// Wasm i64.div_s instruction
+  external int _div_s(int divisor);
 
-/// Wasm i64.le_u instruction
-external bool _le_u(int other);
+  /// Wasm i64.le_u instruction
+  external bool _le_u(int other);
 
-/// Wasm i64.lt_u instruction
-external bool _lt_u(int other);
+  /// Wasm i64.lt_u instruction
+  external bool _lt_u(int other);
 
-/// Wasm i64.shr_s instruction
-external int _shr_s(int shift);
+  /// Wasm i64.shr_s instruction
+  external int _shr_s(int shift);
 
-/// Wasm i64.shr_u instruction
-external int _shr_u(int shift);
+  /// Wasm i64.shr_u instruction
+  external int _shr_u(int shift);
 
-/// Wasm i64.shl instruction
-external int _shl(int shift);
+  /// Wasm i64.shl instruction
+  external int _shl(int shift);
 }
