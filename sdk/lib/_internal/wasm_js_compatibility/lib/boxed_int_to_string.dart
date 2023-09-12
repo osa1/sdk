60aa55976fb1f3f55ef1115e233d0ca12f65de05
Ömer Sinan Ağacan <omersa@google.com>
2023-09-12 10:39:24 +0200

    Remove commented-out code

--- sdk/lib/_internal/wasm_js_compatibility/lib/boxed_int_to_string.dart
+++ sdk/lib/_internal/wasm_js_compatibility/lib/boxed_int_to_string.dart
@@ -2,11 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// @patch
-// String _intToString(int value) {
-//   throw '_intToString in JS compatbility mode (value = $value)';
-// }
-
 import 'dart:_internal';
 import 'dart:_js_helper';
 import 'dart:_js_types';
