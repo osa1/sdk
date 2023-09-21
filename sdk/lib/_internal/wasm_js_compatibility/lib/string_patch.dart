f3af6c9d00988b8c310ed95ca5d548e644c05cf5
Ömer Sinan Ağacan <omersa@google.com>
2023-09-21 10:33:40 +0200

    Remove buggy _fromIterable

--- sdk/lib/_internal/wasm_js_compatibility/lib/string_patch.dart
+++ sdk/lib/_internal/wasm_js_compatibility/lib/string_patch.dart
@@ -14,16 +14,37 @@ class String {
   @patch
   factory String.fromCharCodes(Iterable<int> charCodes,
       [int start = 0, int? end]) {
-    // TODO(joshualitt): Fast path when lists are backed by JS array in JSCM.
-    // TODO(omersa): Fast cases below commented-out for now as they're buggy.
-    // if (charCodes is Uint8List) {
-    //   return _fromJSArrayLike((charCodes as JSUint8ArrayImpl).toExternRef,
-    //       start, end, charCodes.length);
-    // } else if (charCodes is EfficientLengthIterable<int>) {
-    //   return _fromEfficientLengthIterable(
-    //       charCodes, start, end ?? charCodes.length);
-    // }
-    return _fromIterable(charCodes, start, end);
+    final length = charCodes.length;
+    if (start < 0) throw RangeError.range(start, 0, length);
+    if (end != null && end < start) {
+      throw RangeError.range(end, start, length);
+    }
+    if (end != null && end > length) {
+      throw RangeError.range(end, start, length);
+    }
+
+    final end_ = end ?? length;
+
+    final it = charCodes.iterator;
+    for (int i = 0; i < start; i++) {
+      it.moveNext();
+    }
+
+    int index = 0;
+    final list = Uint32List(end_ - start);
+    while (it.moveNext()) {
+      final code = it.current;
+      if (code <= 0xffff) {
+        list[index++] = code;
+      } else if (code <= 0x10ffff) {
+        list[index++] = _computeHigh(code);
+        list[index++] = _computeLow(code);
+      } else {
+        throw ArgumentError('Invalid code point $code');
+      }
+    }
+    return _fromJSArrayLike(
+        (list as JSIntArrayImpl).toExternRef, start, end_, charCodes.length);
   }
 
   @patch
@@ -58,7 +79,7 @@ class String {
   static String _fromCharCodeApplySubarray(WasmExternRef? result,
           WasmExternRef? charCodes, double index, double end) =>
       JSStringImpl(js.JS<WasmExternRef?>(
-          '(r, c, i, e) => String.fromCharCode.apply(null, c.subarray(i, e))',
+          '(r, c, i, e) => String.fromCharCode.apply(null, c.slice(i, e))',
           result,
           charCodes,
           index,
@@ -84,74 +105,4 @@ class String {
       0xd800 + ((((code - 0x10000) >> 10) & 0x3ff));
 
   static int _computeLow(int code) => 0xdc00 + (code & 0x3ff);
-
-  static void _addCodeToArray(JSArrayImpl array, int code) {
-    if (code <= 0xffff) {
-      array.add(code.toJS);
-    } else if (code <= 0x10ffff) {
-      array.add(_computeHigh(code).toJS);
-      array.add(_computeLow(code).toJS);
-    } else {
-      throw ArgumentError('Invalid code point $code');
-    }
-  }
-
-  static String _fromIterable(Iterable<int> charCodes, int start, int? end) {
-    if (start < 0) throw RangeError.range(start, 0, charCodes.length);
-    if (end != null && end < start) {
-      throw RangeError.range(end, start, charCodes.length);
-    }
-
-    final it = charCodes.iterator;
-    for (int i = 0; i < start; i++) {
-      if (!it.moveNext()) {
-        throw RangeError.range(start, 0, i);
-      }
-    }
-
-    final array = JSArrayImpl.empty();
-    if (end == null) {
-      while (it.moveNext()) {
-        _addCodeToArray(array, it.current);
-      }
-    } else {
-      for (int i = start; i < end; i++) {
-        if (!it.moveNext()) {
-          throw RangeError.range(end, start, i);
-        }
-        _addCodeToArray(array, it.current);
-      }
-    }
-    return _fromJSArrayLike(
-        (array as JSArrayImpl).toExternRef, start, end, charCodes.length);
-  }
-
-  static String _fromEfficientLengthIterable(
-      EfficientLengthIterable<int> charCodes, int start, int end) {
-    if (start < 0) throw RangeError.range(start, 0, charCodes.length);
-    if (end < start) {
-      throw RangeError.range(end, start, charCodes.length);
-    }
-
-    final it = charCodes.iterator;
-    for (int i = 0; i < start; i++) {
-      it.moveNext();
-    }
-
-    int index = 0;
-    final list = Uint32List(end - start);
-    while (it.moveNext()) {
-      final code = it.current;
-      if (code <= 0xffff) {
-        list[index++] = code;
-      } else if (code <= 0x10ffff) {
-        list[index++] = _computeHigh(code);
-        list[index++] = _computeLow(code);
-      } else {
-        throw ArgumentError('Invalid code point $code');
-      }
-    }
-    return _fromJSArrayLike(
-        (list as JSIntArrayImpl).toExternRef, start, end, charCodes.length);
-  }
 }
