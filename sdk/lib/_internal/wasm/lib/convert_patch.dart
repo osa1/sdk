0cb3a7bcacea5665d8f3b1cdbdd732c77fcaba46
Ömer Sinan Ağacan <omersa@google.com>
2023-10-04 09:21:16 +0200

    "Fix" chunked JSON decoding by not chunking

--- sdk/lib/_internal/wasm/lib/convert_patch.dart
+++ sdk/lib/_internal/wasm/lib/convert_patch.dart
@@ -1416,7 +1416,29 @@ class _JsonStringParser extends _ChunkedJsonParser<String> {
 class JsonDecoder {
   @patch
   StringConversionSink startChunkedConversion(Sink<Object?> sink) {
-    return new _JsonStringDecoderSink(this._reviver, sink);
+    // return _JsonStringDecoderSink(this._reviver, sink);
+    return _JsonDecoderSink(_reviver, sink);
+  }
+}
+
+/// Implements the chunked conversion from a JSON string to its corresponding
+/// object.
+///
+/// The sink only creates one object, but its input can be chunked.
+// TODO(floitsch): don't accumulate everything before starting to decode.
+class _JsonDecoderSink extends _StringSinkConversionSink<StringBuffer> {
+  final Object? Function(Object? key, Object? value)? _reviver;
+  final Sink<Object?> _sink;
+
+  _JsonDecoderSink(this._reviver, this._sink) : super(StringBuffer(''));
+
+  void close() {
+    super.close();
+    String accumulated = _stringSink.toString();
+    _stringSink.clear();
+    Object? decoded = _parseJson(accumulated, _reviver);
+    _sink.add(decoded);
+    _sink.close();
   }
 }
 
