1f276f5bbc2141cc85c84c8f4ad2f76cf7443efe
Ömer Sinan Ağacan <omersa@google.com>
2023-10-27 12:20:12 +0200

    Fix

--- pkg/dart2wasm/lib/dispatch_table.dart
+++ pkg/dart2wasm/lib/dispatch_table.dart
@@ -237,13 +237,13 @@ class DispatchTable {
   final Map<int, SelectorInfo> _selectorInfo = {};
 
   /// Maps member names to getter selectors with the same member name.
-  final Map<String, List<SelectorInfo>> _dynamicGetters = {};
+  final Map<String, Set<SelectorInfo>> _dynamicGetters = {};
 
   /// Maps member names to setter selectors with the same member name.
-  final Map<String, List<SelectorInfo>> _dynamicSetters = {};
+  final Map<String, Set<SelectorInfo>> _dynamicSetters = {};
 
   /// Maps member names to method selectors with the same member name.
-  final Map<String, List<SelectorInfo>> _dynamicMethods = {};
+  final Map<String, Set<SelectorInfo>> _dynamicMethods = {};
 
   /// Contents of [wasmTable]. For a selector with ID S and a target class of
   /// the selector with ID C, `table[S + C]` gives the reference to the class
@@ -295,7 +295,6 @@ class DispatchTable {
             metadata.methodOrSetterCalledDynamically ||
             member.name.text == "call");
 
-    final newSelector = !_selectorInfo.containsKey(selectorId);
     final selector = _selectorInfo.putIfAbsent(
         selectorId,
         () => SelectorInfo._(translator, selectorId,
@@ -304,13 +303,13 @@ class DispatchTable {
     assert(selector.isSetter == isSetter);
     selector.hasTearOffUses |= metadata.hasTearOffUses;
     selector.paramInfo.merge(paramInfo);
-    if (newSelector && calledDynamically) {
+    if (calledDynamically) {
       if (isGetter) {
-        (_dynamicGetters[member.name.text] ??= []).add(selector);
+        (_dynamicGetters[member.name.text] ??= {}).add(selector);
       } else if (isSetter) {
-        (_dynamicSetters[member.name.text] ??= []).add(selector);
+        (_dynamicSetters[member.name.text] ??= {}).add(selector);
       } else {
-        (_dynamicMethods[member.name.text] ??= []).add(selector);
+        (_dynamicMethods[member.name.text] ??= {}).add(selector);
       }
     }
     return selector;
