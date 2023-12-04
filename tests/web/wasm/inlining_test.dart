697d85f5f29a501d09df59eece8926079b3e4b2e
Ömer Sinan Ağacan <omeragacan@gmail.com>
2023-12-04 15:07:08 +0100

    Remove redundant option

--- tests/web/wasm/inlining_test.dart
+++ tests/web/wasm/inlining_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// dart2wasmOptions=--inlining-limit=10
-
 // Tests that we don't recursively inline.
 
 import 'package:expect/expect.dart';
