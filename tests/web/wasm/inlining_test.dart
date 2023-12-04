3e2e0b008e4a231fb7d068ff6c1458c66b0ed158
Ömer Sinan Ağacan <omeragacan@gmail.com>
2023-12-04 15:07:59 +0100

    Rewording

--- tests/web/wasm/inlining_test.dart
+++ tests/web/wasm/inlining_test.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// Tests that we don't recursively inline.
+// Tests that dart2wasm doesn't recursively inline.
 
 import 'package:expect/expect.dart';
 
