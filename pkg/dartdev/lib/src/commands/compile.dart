b9734920d33810655c72105199700e54b5a6ce53
Ömer Sinan Ağacan <omersa@google.com>
2023-12-06 09:15:26 +0100

    [dart2wasm] Fix wasm-opt path in `dart compile wasm`

--- pkg/dartdev/lib/src/commands/compile.dart
+++ pkg/dartdev/lib/src/commands/compile.dart
@@ -392,7 +392,7 @@ class CompileWasmCommand extends CompileSubcommandCommand {
   static const String help =
       'Compile Dart to a WebAssembly/WasmGC module (EXPERIMENTAL).';
 
-  final String optimizer = path.join(binDir.path, 'utils', 'wasm-opt');
+  final String optimizer = path.join(binDir.path, 'wasm-opt');
   String optimizerFlags(bool outputNames) =>
       '-all --closed-world -tnh --type-unfinalizing -O3 --type-ssa'
       ' --gufa -O3 --type-merging -O1 --type-finalizing'
