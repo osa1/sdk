b497429adfdc9b587377c65e8271d7c8326a873c
Ömer Sinan Ağacan <omersa@google.com>
2024-04-04 12:40:07 +0200

    Add test

---
+++ tests/web/wasm/regress_55347_test.dart
@@ -0,0 +1,45 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:expect/expect.dart';
+
+class MyException implements Exception {
+  MyException([this.message]);
+
+  final String? message;
+
+  @override
+  String toString() => 'MyException($message)';
+}
+
+class MyOtherException implements Exception {
+  MyOtherException([this.message]);
+
+  final String? message;
+
+  @override
+  String toString() => 'MyOtherException($message)';
+}
+
+Future<String> asynchronouslyThrowException() async {
+  throw MyException('throwing an error!');
+}
+
+Future<String?> test() async {
+  try {
+    await asynchronouslyThrowException();
+    Expect.fail('Exception is not thrown');
+  } on MyOtherException {
+    Expect.fail('Wrong exception caught');
+  } on MyException {
+    return 'Success';
+  } catch (error) {
+    Expect.fail('Wrong exception casught');
+  }
+  return null;
+}
+
+void main() async {
+  Expect.equals(await test(), Success);
+}
