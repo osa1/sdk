a186d920461494ac23da68bc7e5c5e5bfda76d09
Ömer Sinan Ağacan <omersa@google.com>
2024-04-08 13:02:18 +0200

    Add a test

---
+++ tests/language/async/nested_test.dart
@@ -0,0 +1,25 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import "package:expect/expect.dart";
+
+Stream<String> stream() async* {
+  yield 'a';
+  yield 'b';
+}
+
+Stream<String> test() async* {
+  final expanded = stream().asyncExpand((s) async* {
+    yield 'before';
+    yield s;
+    yield 'after';
+  });
+
+  yield* expanded;
+}
+
+void main() async {
+  Expect.listEquals(
+      ['before', 'a', 'after', 'before', 'b', 'after'], await test().toList());
+}
