f1a6d2da39705711ba93474138cdea964a99f7fc
Ömer Sinan Ağacan <omersa@google.com>
2024-12-03 13:32:08 +0100

    Simplify

--- pkg/dart2wasm/lib/code_generator.dart
+++ pkg/dart2wasm/lib/code_generator.dart
@@ -2430,6 +2430,9 @@ abstract class AstCodeGenerator
         // A closure in the member is called.
         final Closures memberClosures =
             translator.getClosures(member, findCaptures: true);
+        // Add the closure's enclosing member to the compilation queue if it's
+        // not already in the queue or compiled.
+        translator.functions.getFunction(member.reference);
         final actualClosureId = closureId - 1;
         final lambda = memberClosures.lambdas.values
             .firstWhere((lambda) => lambda.index == actualClosureId);
